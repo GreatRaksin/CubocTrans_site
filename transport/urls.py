36328b803d0101e0a85a023fd63885bb7970972b
ARCHITECT <greatraksin@icloud.com>
2021-08-12 00:04:31 +0300

    почистил импорты в урлах

--- transport/urls.py
+++ transport/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from bus import views
-from bus.views import IndexView, ContactView, contact_view, success_view, index_view
+from bus.views import contact_view, success_view, index_view
 from django.conf.urls.static import static
 from django.conf import settings
 from django.db import models
