3f5b3ab9df3ea744980613325ce91f03c3581e5d
ARCHITECT <greatraksin@icloud.com>
2021-08-11 11:21:50 +0300

    Изменил view для главной страницы

--- bus/admin.py
+++ bus/admin.py
@@ -10,6 +10,7 @@ class ContactAdmin(admin.ModelAdmin):
     list_display = ('company_name', 'phone', 'cargo_name', 'volume', 'cost')
     list_filter = ('company_name', 'phone', 'email', 'cost')
 
+
 class PartnerAdmin(admin.ModelAdmin):
     list_display = ('name', 'link')
 
